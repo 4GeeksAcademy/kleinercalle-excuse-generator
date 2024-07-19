956f56db94fc43dbc4fa9883a948436cf7795c9d
kleinercalle <kleiner2024@gmail.com>
2024-07-19 23:13:26 +0000

    pretier

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indexwho = Math.floor(Math.random() * who.length);
+  let indexAction = Math.floor(Math.random() * action.length);
+  let what;
 };
