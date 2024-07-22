1e5cb6aec85a2f6ea67e767b48e1e0ed115b5ca6
kleinercalle <kleiner2024@gmail.com>
2024-07-22 10:11:47 +0000

    hice la excusa

--- src/app.js
+++ src/app.js
@@ -7,21 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-
+  
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  let when = ["before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying"];
+
   let indexwho = Math.floor(Math.random() * who.length);
   let indexaction = Math.floor(Math.random() * action.length);
   let indexwhat = Math.floor(Math.random() * what, length);
   let indexwhen = Math.floor(Math.random() * when.length);
-  let subtitulo = document.querySelector("#excusa");
-  subtitulo.innerHTML = who[indexwho];
+
+  let excusa = who[indexwho] + ' ' + action[indexaction] + ' ' + what[indexwhat] + ' ' + when[indexwhen];
+
+
+  const textexcuse = document.querySelector('#excuse');
+  textexcuse.innerHTML = excusa;
 };
